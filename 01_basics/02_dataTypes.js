78ee137870763f7614dae0221455ea3e2388fa31
DeveshNagar07 <devesh1766@gmail.com>
2024-07-24 16:24:35 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all Js code as newer version or js.node 
+
+// alert(3+3) //we are using nodejs, not browser 
+
+console.log(3
+     + 
+    3); //code readability should be hight 
+
+console.log("devesh")
+
+
+let name = "devesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// types of dataType
+//number => 2 to power 53
+//bigint
+//string => ""
+// Boolean => true/false
+// null => standalone value ye ek object h ##interview question
+// undefind =>   undefind ek undefind hi hai ##interview question
+// Symbol => unique 
+
+// Object
+
+console.log(typeof undefined);
